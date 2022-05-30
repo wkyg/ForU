a91414f9d0aa88bd498c4a6eddefbae895800e8f
Kelvin-Desktop <39939811+wkyg@users.noreply.github.com>
2022-05-30 17:46:39 +0800

    new activity
    -imported resources

--- app/src/main/java/com/example/foru/MainActivity.java
+++ app/src/main/java/com/example/foru/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.foru;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                //Code for test another activity pages.
+                finish();
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+
+        }, 3000L);
     }
 }
