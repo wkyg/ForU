3f2b0a3d0589ebace1eb31a429f339de2d89db55
WK Yong <39939811+wkyg@users.noreply.github.com>
2022-06-13 17:57:31 +0800

    onclick added
    Clicking on the picture will now change to next picture

--- app/src/main/java/com/example/foru/DetailActivity.java
+++ app/src/main/java/com/example/foru/DetailActivity.java
@@ -9,6 +9,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.protobuf.StringValue;
 import com.google.type.DateTime;
@@ -26,6 +27,11 @@ public class DetailActivity extends AppCompatActivity {
     public String CurrentDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
     public String DiffDate;
     public String LoveEmoji;
+    public int upperbound = 13;
+    public int randInt;
+    public int imageResource;
+    public String uri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,32 +44,43 @@ public class DetailActivity extends AppCompatActivity {
 
         //get emoji
         LoveEmoji = getEmojiByUnicode(0x2764);
-        //Get random image
+
+        //Get random image //total number of pictures 0-x
         Random rand = new Random();
-        //total number of pictures 0-x
-        int upperbound = 13;
-        int randInt = rand.nextInt(upperbound);
-        String uri = "@drawable/tgt"+randInt;
-        int imageResource = getResources().getIdentifier(uri, null, getPackageName());
-        Drawable res = getResources().getDrawable(imageResource);
+        randInt = rand.nextInt(upperbound);
+        uri = "@drawable/tgt"+randInt;
+        imageResource = getResources().getIdentifier(uri, null, getPackageName());
+        Drawable res = getDrawable(imageResource);
+
         //Set Texts
         TextView kk = findViewById(R.id.WkTxt);
         kk.setText("怪叔叔 / 慨慨" + LoveEmoji);
+
         //Set Texts
         TextView yl = findViewById(R.id.YlTxt);
         yl.setText("小小粒 / 铃铃" + LoveEmoji);
+
         //Set Texts
         TextView tgt = findViewById(R.id.TgtTxt);
         tgt.setText("我们在一起");
+
         //Set Image
         ImageView img = findViewById(R.id.ImageTgt);
         img.setImageDrawable(res);
-        //On click change picture (seek)
-        img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
+        //On click change picture (seek)
+        img.setOnClickListener(v -> {
+            if (randInt >= 12){
+                randInt = 0;
+            }else{
+                randInt++;
             }
+
+            uri = "@drawable/tgt"+randInt;
+            imageResource = getResources().getIdentifier(uri, null, getPackageName());
+            Drawable newRes = getDrawable(imageResource);
+            ImageView img1 = findViewById(R.id.ImageTgt);
+            img1.setImageDrawable(newRes);
         });
 
         //Date calculations
@@ -80,7 +97,7 @@ public class DetailActivity extends AppCompatActivity {
             TxtDays.setText(DiffDate + "days");
         }catch(Exception exception){
             TextView TxtDays = findViewById(R.id.TxtDays);
-            TxtDays.setText("Please contact ur bf for help");
+            TxtDays.setText("Call ur bf for help");
         }
     }
 
